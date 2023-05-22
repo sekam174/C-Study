06b31423d48a489f6e800c3b75c9458d1c8c416d
sergey ledenkov <sergey1978-174@mail.ru>
2023-05-22 14:07:21 +0300

    added Seminar1 dz

---
+++ Seminar1_dz/task02/Program.cs
@@ -0,0 +1,23 @@
+﻿// напишите программу, которая
+// на вход принимает два числа и
+// выдаёт какое число больше, а какое меньше
+
+// a = 5; b = 7 -> max = 7
+// a = 9; b = 2 -> max = 9
+
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+    int max = a;
+    Console.WriteLine($"max -> {a}");
+}
+else if(b > a)
+{
+    int max = b;
+    Console.WriteLine($"max -> {b}");
+}
