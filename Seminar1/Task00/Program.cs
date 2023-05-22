608b0b485a165583e3aa41b7acee45a2e232ed85
sergey ledenkov <sergey1978-174@mail.ru>
2023-05-22 11:30:23 +0300

    added Task 00

---
+++ Seminar1/Task00/Program.cs
@@ -0,0 +1,13 @@
+﻿// напишите программу, которая
+// на вход принимает число и
+// выдаёт его квадрат(умноженное на само себя)
+
+// 4 -> 16
+// 3 -> 9
+// -7 -> 49
+
+Console.WriteLine("Введие число");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = num * num;
+Console.WriteLine(num2);
+
