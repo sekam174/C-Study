47a697b12f8d80c46bd74c797796aa74270176e2
sergey ledenkov <sergey1978-174@mail.ru>
2023-05-22 16:04:48 +0300

    added task 06

---
+++ Seminar1_dz/task06/Program.cs
@@ -0,0 +1,23 @@
+﻿// напишите программу, которая
+// на вход принимаетчисло и выдаёт
+// является ли число чётным(делится бе остатка)
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    int evenNum = num % 2;
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+
