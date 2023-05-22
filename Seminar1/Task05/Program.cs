9887808f51a7928619bb63e6714242427e876238
sergey ledenkov <sergey1978-174@mail.ru>
2023-05-22 12:56:52 +0300

    added Task 05

---
+++ Seminar1/Task05/Program.cs
@@ -0,0 +1,17 @@
+﻿// напишите программу, которая
+// на вход прининимает одно число(N)
+// а на выходе показывает все целые числа
+// в промежутке от (-N) до (N)
+
+// 4 -> "-4,-3,-2,-1,0,1,2,3,4"
+// 2 -> "-2,-1,0,1,2"
+
+Console.WriteLine("Введите целое натуральное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int negNumber = -1 * num;
+while(negNumber <= num)
+{
+    Console.Write(negNumber +" ");
+    negNumber = negNumber + 1;
+}
