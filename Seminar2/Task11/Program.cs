d047bbe5ea3c53441eea8190cf11476587716b03
sergey ledenkov <sergey1978-174@mail.ru>
2023-05-24 13:49:49 +0300

    added Task 11

---
+++ Seminar2/Task11/Program.cs
@@ -0,0 +1,17 @@
+﻿// напишите программу, которая выводит
+// случайное трёхзначное число и удаляет
+// вторую цифру этого числа
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+
+int num1 = num / 100;
+int num2 = num % 10;
+
+int result = num1 * 10 + num2;
+
+Console.WriteLine(result);
