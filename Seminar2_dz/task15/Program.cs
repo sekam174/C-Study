78ef652a7916d8418fcac9cc724255f7b2884c70
sergey ledenkov <sergey1978-174@mail.ru>
2023-05-26 10:42:21 +0300

    added task 15

---
+++ Seminar2_dz/task15/Program.cs
@@ -0,0 +1,22 @@
+﻿// напишите программу, которая принимает на вход
+// цифру, обозначающую день недели и проверяет
+// является ли этот день выходным
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// Console.WriteLine("Введите день недели");
+// int day = Convert.ToInt32(Console.ReadLine());
+
+int day = new Random().Next(1, 8);
+Console.WriteLine(day);
+
+if((day % 6 == 0) || (day % 7 == 0))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
