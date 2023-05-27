b72977c94b5eb3f701c1e5e51f0ea1b5f2eacab2
sergey ledenkov <sergey1978-174@mail.ru>
2023-05-27 21:04:46 +0300

    added Task 22

---
+++ Sminar3/Task22/Program.cs
@@ -0,0 +1,22 @@
+﻿// напишите программу, которая принимает
+// на вход число (N) и выдаёт
+// таблицу квадратов чисел от 1 до N
+//
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1, 4.
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+if(count > num)
+{
+    Console.WriteLine("Не соответствует условиям");
+}
+
+while(count <= num)
+{
+    Console.Write(Math.Pow(count, 2)+ " ");
+    count = count + 1;
+}
