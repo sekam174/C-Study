001f52b70ecbd54847e1a22f5c7974420754b0c4
sergey ledenkov <sergey1978-174@mail.ru>
2023-05-31 15:48:13 +0300

    added Task 26

---
+++ Seminar4/Task26/Program.cs
@@ -0,0 +1,18 @@
+﻿// напишите программу, которая
+// принимает на вход число и
+// выдаёт количество цифр в числе
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите число");
+string text = Console.ReadLine();
+Console.WriteLine(Length());
+int Length()
+{
+    int length = text.Length;
+    return length;
+}
+
+// Console.WriteLine(Length());
