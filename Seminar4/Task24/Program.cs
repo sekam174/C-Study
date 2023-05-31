660f9f9d69aced45a26510c0727a5df90cc79a9b
sergey ledenkov <sergey1978-174@mail.ru>
2023-05-31 15:09:45 +0300

    added Task 24

---
+++ Seminar4/Task24/Program.cs
@@ -0,0 +1,26 @@
+﻿// напишите программу, которая
+// принимает на вход число (А) и
+// выдаёт сумму чисел от 1 до А
+
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int GetSumNumber()
+{
+    int sum = 0;
+    int count = 1;
+
+    while(count <= num)
+    {
+        sum = sum + count;
+        count = count + 1;
+    }
+    return sum;
+}
+
+int result = GetSumNumber();
+Console.WriteLine(result);
