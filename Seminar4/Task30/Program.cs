6f9d33c002d4b2ef66834aafa1ef3c878734ca25
sergey ledenkov <sergey1978-174@mail.ru>
2023-05-31 17:16:58 +0300

    added Task 30

---
+++ Seminar4/Task30/Program.cs
@@ -0,0 +1,19 @@
+﻿// напишите программу, которая
+// выводит массив из 8 элементов
+// заполненный 0 и 1 в случайном порядке
+
+// [ 1, 0, 1, 1, 0, 0, 1, 0 ]
+
+void CreateArray()
+{
+    int[] arr = new int[8];
+    int count = 0;
+
+    while(count < arr.Length)
+    {
+        arr[count] = new Random().Next(0, 2);
+        Console.Write($"{arr[count]} ");
+        count++;
+    }
+}
+CreateArray();
