96e8142b7f03c743f6532f7cb0618d2043a8bef8
sergey ledenkov <sergey1978-174@mail.ru>
2023-05-31 16:31:44 +0300

    added Task 28

---
+++ Seminar4/Task28/Program.cs
@@ -0,0 +1,26 @@
+﻿// напишите программу, которая
+// принимает на вход число (N)
+// и выдаёт произведение чисел от 1 до N
+
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Div());
+
+int Div()
+{
+    int result = 1;
+    int count = 1;
+
+    while(count <= num)
+    {
+        result = result * count;
+        count++;
+    }
+    return result;
+}
+
+// Console.WriteLine(Div());
