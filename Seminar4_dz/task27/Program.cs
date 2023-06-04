4740371f002b5b64495b1b8adca77d17e5dab0ed
sergey ledenkov <sergey1978-174@mail.ru>
2023-06-04 15:48:50 +0300

    added task 27

---
+++ Seminar4_dz/task27/Program.cs
@@ -0,0 +1,21 @@
+﻿// напишите программу, которая принимает
+// на вход число и выдаёт сумму цифр числа
+
+// 452 -> 11
+// 82  -> 10
+//9012 -> 12
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Sum()
+{
+    int result = 0;
+    while(num > 0)
+    {
+        result = result + num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+Console.WriteLine($"Сумма цифр числа {Sum()}");
