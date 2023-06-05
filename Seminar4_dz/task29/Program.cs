444e6965e97b324aa85b2f06d9d1518099d2be21
sergey ledenkov <sergey1978-174@mail.ru>
2023-06-05 13:10:17 +0300

    added task 29

---
+++ Seminar4_dz/task29/Program.cs
@@ -0,0 +1,23 @@
+﻿// напишите программу, которая задаёт
+// массив из 8 элеменов и выводит их на экран
+
+// 1, 2, 5, 7, 13 -> [1, 3, 5, 7, 13]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+void CreateArray()
+{
+    int[] arr = new int[8];
+    int count = 0;
+
+    Console.Write("[");
+
+    while(count < arr.Length )
+    {
+        arr[count] = new Random().Next(1, 10);
+        Console.Write($"{ arr[count]} ");
+        count++;   
+    } 
+    Console.Write("]");
+}
+CreateArray();
