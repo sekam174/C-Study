5cbd1651692ccaafa9f6b61611c8d2acb00824e1
sergey ledenkov <sergey1978-174@mail.ru>
2023-06-10 15:01:38 +0300

    added Task 32

---
+++ Seminar5/Taask32/Program.cs
@@ -0,0 +1,21 @@
+﻿// напишите программу замены элементов массива...
+// положительные элементы заменить на
+// соответствующие отрицательные и наооборот.
+
+// [-4, -8, 3, 2] -> [4, 8, -3, -2]
+
+int[] array = new int[4];
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-8, 9);
+    Console.Write(array[i]+" ");
+}
+
+Console.WriteLine();
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = -array[i];
+    Console.Write(array[i]+" ");
+}
