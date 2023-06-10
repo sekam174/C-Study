2bf4726e83338576035360bd7572b86d85eec304
sergey ledenkov <sergey1978-174@mail.ru>
2023-06-10 21:36:48 +0300

    added Task 33

---
+++ Seminar5/Task33/Program.cs
@@ -0,0 +1,52 @@
+﻿// задайте массив...напишите программу, которая определяет
+// присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+void FillArray(int[]collection)
+{
+    int length  = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[]col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int IndexOf(int[]collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index < count)
+    {
+        if(collection[index] == find) // find - находить
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+int[] array = new int[10];
+FillArray(array);
+
+array[4] = 4;
+
+
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
