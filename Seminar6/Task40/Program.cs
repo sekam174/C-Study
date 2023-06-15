d5369e88a5825cecc09c83aa6dc6634cd13106fb
sergey ledenkov <sergey1978-174@mail.ru>
2023-06-15 22:13:18 +0300

    added Task 40

---
+++ Seminar6/Task40/Program.cs
@@ -0,0 +1,25 @@
+﻿// напишите программу, которая принимает
+// 3 числа и проверяет, может ли
+// существовать треугольник со
+// сторонами такой длины
+
+// ТЕОРЕМА О НЕРАВЕНСТВЕ ТРЕУГОЛЬНИКА: Каждая сторона
+// треугольника меньше суммы двух других сторон;
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int c = int.Parse(Console.ReadLine());
+
+if((a < b + c)&&(b < a + c)&&(c < a + c))
+{
+    Console.WriteLine("Может");
+}
+else
+{
+    Console.WriteLine("Числа не подходят");
+}
