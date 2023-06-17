f43f7af33ec081808ffadcf1c978d369f2dc2530
sergey ledenkov <sergey1978-174@mail.ru>
2023-06-17 13:05:21 +0300

    added Task 44

---
+++ Seminar6/Task44/Program.cs
@@ -0,0 +1,24 @@
+﻿// не используя рекурсию, выведите
+// первые N чисел Фибоначи,
+// первые два числа Фибоначи: 0 и 1
+
+// Если N = 5 -> 01123
+// Если N = 3 -> 011
+// Если N = 7 -> 0112358
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int firstNum = 0;
+int secondNum =1;
+
+Console.Write(firstNum+" " + secondNum+" ");
+
+for(int i = 3; i <= num; i++)
+{
+    int newNum = firstNum + secondNum;
+    Console.Write(newNum+" ");
+
+    firstNum = secondNum;
+    secondNum = newNum;
+}
