5ed9c51e7d63f3d81001b982ec10f6c7d519f4ee
sergey ledenkov <sergey1978-174@mail.ru>
2023-06-19 21:28:55 +0300

    added task 41_1

---
+++ Seminar6_dz/task41_1/Program.cs
@@ -0,0 +1,29 @@
+﻿// пользователь вводит с клавы М чисел.
+// посчитайте, сколько чисел больше 0 ввёл пользователь
+
+// 0, 7, 8, -2, -2  -> 2
+//-1, -7,  56, 89, 22 -> 3
+
+// Понравилась Ваша фишка
+
+int result = 0;
+string exitSim = "$";
+
+int Kol()
+{
+    Console.Write("Введите число; если хотите выйти, нажмите " + exitSim);
+    string sim = Console.ReadLine();
+
+    if(sim == exitSim)
+    {
+        return result;
+    }
+    int sum = Convert.ToInt32(sim);
+    if(sum > 0)
+    {
+        result += 1;
+    }
+    Console.WriteLine($"{ result}");
+    return Kol();
+}
+Kol();
