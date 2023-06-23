977dc4a108808d88d1904d804570d59f13a0e1ff
sergey ledenkov <sergey1978-174@mail.ru>
2023-06-23 18:15:26 +0300

    added Task 59

---
+++ Seminar8/Task59/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задайте двумерный массив из целых чисел.
+// напишите программу, которая удалит строку и столбец,
+// на пересечении которых расположен наименьший элт массива
+
+Console.WriteLine("Введите количество строчек");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+int[,]array = new int[rows,rows];
+int minVulue = int.MaxValue;
+
+int minIndexRows = 0;
+int minIndexColumns = 0;
+
+void GetArray()
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(0,10);
+            if(array[i,j] < minVulue)
+            {
+                minVulue = array[i,j];
+                minIndexRows = i;
+                minIndexColumns = j;
+            }
+        }
+    }
+}
+void PrintArray(int[,]result)
+{
+    for(int i = 0; i < result.GetLength(0); i++)
+    {
+        for(int j = 0; j < result.GetLength(1); j++)
+        {
+            Console.Write(result[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillNewArray()
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        if(i != minIndexRows)
+        {
+            for(int j = 0; j < array.GetLength(1); j++)
+            {
+                if(j != minIndexColumns)
+                {
+                    Console.Write(array[i,j]);
+                }
+            }
+        }
+        Console.WriteLine();
+    }
+}
+GetArray();
+PrintArray(array);
+Console.WriteLine();
+FillNewArray();
+Console.WriteLine($"Мин. знач.:{minVulue},индексы{minIndexRows},{minIndexColumns}");
