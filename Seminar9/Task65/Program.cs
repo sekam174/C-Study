05d8640bffa67f26c33fce45a8f7c993993eed0c
sergey ledenkov <sergey1978-174@mail.ru>
2023-06-24 21:25:00 +0300

    added Task 65

---
+++ Seminar9/Task65/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значения М и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от М до N
+
+// M = 1; N = 5 -> " 1, 2, 3, 4, 5 "
+// M = 4; N = 8 -> " 4, 5, 6, 7, 8 "
+
+Console.WriteLine("Введите число М");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string PrintNum(int num1, int num2)
+{
+    if(num1 > num2)
+    {
+        return "нет решения";
+    }
+    if(num1 == num2)
+    {
+        return num1.ToString();
+    }
+    return(num1+" "+PrintNum(num1+1,num2));
+}
+Console.WriteLine(PrintNum(num, number));
