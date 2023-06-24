448d5e00c244990867a376723cf07ef204155cff
sergey ledenkov <sergey1978-174@mail.ru>
2023-06-24 16:57:13 +0300

    added Task 63

---
+++ Seminar9/Task63/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значение N. Напишите програамму,которая выведет
+// все натуральные числа в промежутке от 1 до N
+
+// N = 5 -> " 1, 2, 3, 4, 5 "
+// N = 6 -> " 1, 2, 3, 4, 5, 6 "
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int startNum = 1;
+
+Console.WriteLine(PrintNumbers(startNum, num));
+
+string PrintNumbers(int num1, int num2)
+{
+    if(num1 == num2)
+    {
+        return num1.ToString();
+    }
+    return(num1+" "+PrintNumbers(num1 + 1, num2));
+}
